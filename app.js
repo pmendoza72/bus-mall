5166095d0406075bc27a37114bef1965a3ae2999
pmendoza72 <pablo_mendoza@hotmail.com>
2017-02-23 20:50:49 -0800

    added images, css, index

---
+++ app.js
@@ -0,0 +1,25 @@
+var productImageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog_duck', 'dragon', 'pen', 'pet_sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water_can', 'wine_glass'];
+var productsArray = [];
+
+function Products(name, path) {
+  this.name = name;
+  this.path = path;
+  this.shown = 0;
+  this.clicks = 0;
+}
+
+// a simple IIFE to build all the product images
+(function() {
+  // build the objects
+})()
+
+// Object Literal for managing functionality
+// of app
+
+var tracker = {
+  // lots of properties and methods
+}
+
+someEl.addEventListener('click', function(e) {
+  // does some stuff on click
+})
