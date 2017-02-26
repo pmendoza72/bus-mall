62767b74724b086b81c2d6631d203e7174fb754c
pmendoza72 <pablo_mendoza@hotmail.com>
2017-02-25 17:18:51 -0800

    added random js

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
+// Array of images
+var productsArray = [];
 var productImageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog_duck', 'dragon', 'pen', 'pet_sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water_can', 'wine_glass'];
 
-// for (var i = 0; i < productImageNames.length ; i++) {
-//   var newEl = document.createElement('td');
-//   var elem = document.createElement('img');
-//   elem.src = 'img/' + productImageNames[i] + '.jpg';
-//   newEl.appendChild(elem);
-//   document.getElementById('picture').appendChild(newEl).appendChild(elem);
-// }
+// 3 Random Images
+function randomImage() {
+  return Math.floor(Math.random() * productImageNames.length);
+};
 
 
-var displayImage = function() {
-    return Math.floor(Math.random() * productImageNames.length);
+function drawPictures() {
+  for (var i = 0; i < 3; i++) {
+    var newEl = document.createElement('td');
+    var elem = document.createElement('img');
+
+    var randIndex = randomImage();
+    console.log('random index: ', randIndex);
+
+    var randProduct = productsArray[randIndex];
+    console.log('random product: ', randProduct);
+
+    elem.src = randProduct.path;
+    console.log('random path: ', elem.src);
+
+    newEl.appendChild(elem);
+    document.getElementById('picture').appendChild(newEl).appendChild(elem);
+
+  }
 }
 
 
-var newEl = document.createElement('td');
-var elem = document.createElement('img');
-elem.src = 'img/' + displayImage() + '.jpg';
-newEl.appendChild(elem);
-document.getElementById('picture').appendChild(newEl).appendChild(elem);
+function Products(name, path) {
+  this.name = name;
+  this.path = path;
+  this.shown = 0;
+  this.clicks = 0;
+}
+
+for (var i = 0; i < productImageNames.length; i++) {
+  var product = new Products(productImageNames[i], 'img/' + productImageNames[i] + '.jpg');
+  productsArray.push(product);
+}
+console.log(productsArray);
 
 
-//
-// this.customersPerHour = function(min, max) {
-//     return Math.floor(Math.random() * productImageNames.length);
-//   };
-//
-//
-//   this.customersPerHour = function(min, max) {
-//       return Math.floor(Math.random() * (max - min + 1)) + min;
-//     };
 
-// var productsArray = [];
-//
-// function Products(name, path) {
-//   this.name = name;
-//   this.path = path;
-//   this.shown = 0;
-//   this.clicks = 0;
-// }
 //
 // // a simple IIFE to build all the product images
 // (function() {
@@ -55,3 +60,5 @@ document.getElementById('picture').appendChild(newEl).appendChild(elem);
 // someEl.addEventListener('click', function(e) {
 //   // does some stuff on click
 // })
+
+drawPictures();
